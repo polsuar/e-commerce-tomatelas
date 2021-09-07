9b58b8f3b7d3197be0dc04e2ef2b5e2485a87c2d
o0juano0o <juam_92@hotmail.com>
2021-09-07 18:42:22 -0300

    preparando seed

--- api/server.js
+++ api/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const { User } = require("./models");
+const routes = require("./routes");
+
+// Load .env variables
 require("dotenv").config();
 
 app.use(express.json);
 
-const routes = require("./routes");
+//Routes
 app.use("/api", routes);
 
 const PORT = process.env.PORT || 3001; // vale la pena un dotEnv?
