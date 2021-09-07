c3a7107e10b6a4a419f35117232749d1f5736ed9
o0juano0o <juam_92@hotmail.com>
2021-09-07 18:25:04 -0300

    models de users y products

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const { User } = require("./models");
+require("dotenv").config();
 
 app.use(express.json);
 
