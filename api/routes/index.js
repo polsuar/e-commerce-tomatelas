ca0a9d0da869375a526a5aa044aa4b924f50f610
o0juano0o <82469028+o0juano0o@users.noreply.github.com>
2021-09-07 21:27:26 -0300

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {User} = require("./models");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const auth = require("../config/auth");
 
