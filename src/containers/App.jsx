3a77acf9cee43d11170561a699c697e25d29c705
abigail-salas <salasmedina02@gmail.com>
2021-09-08 19:39:52 -0300

    Single Product agregado

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import Prueba from "../components/Prueba";
-import Cervezas from "../components/Cervezas";
+import SingleProductView from "../components/SingleProductView";
+import Ranceada from "../components/ranceada";
 
 export default function App() {
   return (
     <div>
       <h1>Bienvenidos al e-commerce de Tomatelas! </h1>
-      <Cervezas />
+      <SingleProductView />
     </div>
   );
 }
