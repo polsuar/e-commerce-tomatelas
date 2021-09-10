6b4ab5656fd808444121a188364197dffe9b8cc6
o0juano0o <juam_92@hotmail.com>
2021-09-09 23:33:44 -0300

    probando tabla favoritos2

--- api/models/index.js
+++ api/models/index.js
@@ -2,11 +2,10 @@ const User = require("./UsersModel");
 const Product = require("./ProductsModel");
 const Cart = require("./CartModel");
 const PayMethod = require("./PayMethodsModel");
-const Favorite = require("./FavoritesModel");
 const Categories = require("./CategoryModel");
 const Orders = require("./OrdersModel");
 
-User.belongsToMany(Product, { through: Favorites });
+User.belongsToMany(Product, { through: "Favorites" });
 
 User.hasOne(Cart, {
   foreignKey: "user_id",
