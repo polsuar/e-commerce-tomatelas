051ff1e1165aa8e1909652e6803dc0f2c5a9a0d4
Pol Suar <polsuar88@gmail.com>
2021-09-17 02:57:22 -0300

    vista editor producto

--- src/components/AdminSingleProduct.jsx
+++ src/components/AdminSingleProduct.jsx
@@ -5,7 +5,7 @@ import { useInput } from "../hooks/useInput";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { useDispatch, useSelector } from "react-redux";
 import { editProduct } from "../store/products";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import {
   Button,
@@ -20,26 +20,29 @@ import {
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
-    width: "90vw",
+    width: "100vw",
     heigth: "100vh",
     display: "flex",
     // marginTop: theme.spacing(1),
     //justifyContent: "space-evenly",
     // background: "aqua",
   },
-  imagenContainer: {
-    width: "50%",
-    heigth: "100%",
-    marginTop: theme.spacing(5),
-    marginLeft: theme.spacing(5),
-    // background: "red",
+  otherMargin: {
+    marginTop: theme.spacing(10),
+    marginLeft: theme.spacing(10),
+    marginRight: theme.spacing(10),
   },
-  textoContainer: {
+  imagenContainer: {
     width: "50%",
     heigth: "100%",
     // marginTop: theme.spacing(5),
     // marginRight: theme.spacing(10),
-    // background: "#CCFAE1",
+  },
+  textoContainer: {
+    width: "50%",
+    heigth: "100%",
+    // marginLeft: theme.spacing(5),
+    marginTop: theme.spacing(10),
   },
 }));
 
@@ -51,9 +54,7 @@ const AdminSingleProduct = ({ id }) => {
   const classes = useStyles();
 
   useEffect(() => {
-    dispatch(getSelectedProduct(id))
-    .then((res)=>
-    
+    dispatch(getSelectedProduct(id)).then((res) =>
       setEdit({
         name: res.payload.name,
         volume: res.payload.volume,
@@ -62,10 +63,10 @@ const AdminSingleProduct = ({ id }) => {
         img: res.payload.img,
         price: res.payload.price,
         category: res.payload.category,
-    })
-  )
-}, [])
-  
+      })
+    );
+  }, []);
+
   const handleChange = (e) => {
     const { id, value } = e.target;
     setEdit((edit) => ({ ...edit, [id]: value }));
@@ -73,7 +74,7 @@ const AdminSingleProduct = ({ id }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let data={
+    let data = {
       name: edit.name,
       volume: edit.volume,
       brand: edit.brand,
@@ -81,117 +82,114 @@ const AdminSingleProduct = ({ id }) => {
       img: edit.img,
       price: edit.price,
       category: edit.category,
-    }
-    dispatch(editProduct(product.id)
-    );
-    history.push("/admin")
+    };
+    dispatch(editProduct(product.id));
+    history.push("/admin");
   };
 
-
   return (
-    <div>
-      <Box>
-        <Grid item xs={12} sm={6}>
-          <Box>
+    <div className={classes.otherMargin}>
+      <Container>
+        <Grid className={classes.mainContainer}>
+          <Grid className={classes.textoContainer}>
             <Typography variant="h4" gutterBottom>
               Producto
             </Typography>
-          </Box>
-          <form onSubmit={handleSubmit} >
-            <Grid container spacing={5}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.name}
-                  onChange={handleChange}
-                  id="name"
-                  label="Nombre"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.volume}
-                  onChange={handleChange}
-                  id="volume"
-                  label="Volumen"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.brand}
-                  onChange={handleChange}
-                  id="brand"
-                  label="Marca"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.stock}
-                  onChange={handleChange}
-                  id="stock"
-                  label="Stock"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.img}
-                  onChange={handleChange}
-                  id="img"
-                  label="Imagen url"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  value={edit.price}
-                  onChange={handleChange}
-                  id="price"
-                  label="Precio"
-                  fullWidth
-                />
-              </Grid>
-              <Box>
-                <Typography variant="h6" gutterBottom>
-                  Categoría :
-                </Typography>
-              </Box>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-              >
-                <MenuItem value={"Cervezas"}>Cervezas</MenuItem>
-                <MenuItem value={"Gaseosas"}>Gaseosas</MenuItem>
-                <MenuItem value={"Aguas"}>Aguas</MenuItem>
-                <MenuItem value={"Aguas Saborizadas"}>
-                  Aguas Saborizadas
-                </MenuItem>
-                <MenuItem value={"Bebidas Energizantes"}>
-                  Bebidas Energizantes
-                </MenuItem>
-                <MenuItem value={"Bebidas Isotónicas"}>
-                  Bebidas Isotónicas
-                </MenuItem>
-                <MenuItem value={"Vinos"}>Vinos</MenuItem>
-                <MenuItem value={"Leches"}>Leches</MenuItem>
-              </Select>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Confirmar
-              </Button>
+            <Grid>
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.name}
+                      onChange={handleChange}
+                      id="name"
+                      label="Nombre"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.volume}
+                      onChange={handleChange}
+                      id="volume"
+                      label="Volumen"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.brand}
+                      onChange={handleChange}
+                      id="brand"
+                      label="Marca"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.stock}
+                      onChange={handleChange}
+                      id="stock"
+                      label="Stock"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.img}
+                      onChange={handleChange}
+                      id="img"
+                      label="Imagen url"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      value={edit.price}
+                      onChange={handleChange}
+                      id="price"
+                      label="Precio"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Box>
+                    <Typography variant="h6" gutterBottom>
+                      Categoría :
+                    </Typography>
+                  </Box>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                  >
+                    <MenuItem value={"Cervezas"}>Cervezas</MenuItem>
+                    <MenuItem value={"Gaseosas"}>Gaseosas</MenuItem>
+                    <MenuItem value={"Aguas"}>Aguas</MenuItem>
+                    <MenuItem value={"Aguas Saborizadas"}>
+                      Aguas Saborizadas
+                    </MenuItem>
+                    <MenuItem value={"Bebidas Energizantes"}>
+                      Bebidas Energizantes
+                    </MenuItem>
+                    <MenuItem value={"Bebidas Isotónicas"}>
+                      Bebidas Isotónicas
+                    </MenuItem>
+                    <MenuItem value={"Vinos"}>Vinos</MenuItem>
+                    <MenuItem value={"Leches"}>Leches</MenuItem>
+                  </Select>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                  >
+                    Confirmar
+                  </Button>
+                </Grid>
+              </form>
             </Grid>
-          </form>
-        </Grid>
-      </Box>
-      <Container>
-      <Grid className={classes.mainContainer}>
-            <Grid className={classes.imagenContainer}>
+          </Grid>
+
+          <Grid className={classes.imagenContainer}>
               <img
                 src={edit.img}
                 alt={edit.name}
@@ -201,8 +199,8 @@ const AdminSingleProduct = ({ id }) => {
                   // marginLeft: theme.spacing(20),
                 }}
               />
-            </Grid>
-      </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
